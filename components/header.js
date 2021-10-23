9db86625a7fded9475fe9057c45dc482954ee6ba
yokotadaiki <yd43star@gmail.com>
2021-10-23 13:05:07 +0900

    ChakraUIを導入した

---
+++ components/header.js
@@ -0,0 +1,39 @@
+import { Heading, Flex, Spacer,  Wrap, WrapItem } from "@chakra-ui/react"
+import Link from 'next/link'
+
+export default function Header() {
+  return (
+    <>
+      <Flex
+        as="nav"
+        bg="teal.400"
+        color="gray.50"
+        padding={{ base: 3, md: 4 }}
+        mb="5"
+      >
+        <Heading
+          as="h1"
+          fontSize={{ base: "md", md: "lg" }}
+          ml="5"
+        >
+          <Link href="/">
+            My Portfolio
+          </Link>
+        </Heading>
+        <Spacer/>
+        <Wrap mr="10">
+          <WrapItem>
+            <Link href="/blog">
+              <a>Blog</a>
+            </Link>
+          </WrapItem>
+          <WrapItem>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </WrapItem>
+        </Wrap>
+      </Flex>
+    </>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react"
+import { Container, Heading, Flex, Spacer } from "@chakra-ui/react"
+import Header from "../components/header"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Header />
+      <Container maxW="container.md">
+        <Component {...pageProps} />
+      </Container>
+    </ChakraProvider>
+  )
 }
-
 export default MyApp
