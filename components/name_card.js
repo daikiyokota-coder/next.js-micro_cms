4ecf441d6f3410da2a596c29c4a6627c14b93328
yokotadaiki <yd43star@gmail.com>
2021-10-24 01:39:11 +0900

    topページの要素をコンポーネント分割

---
+++ components/name_card.js
@@ -0,0 +1,36 @@
+import { Box, Center, Image, Text } from "@chakra-ui/react"
+export default function NameCard() {
+  return (
+    <Box
+      borderWidth="1px"
+      borderRadius="lg"
+      w="100%"
+      h="350px"
+      boxShadow="md"
+      p="6"
+      mt="8"
+      rounded="md"
+    >
+      <Center>
+        <Image
+          borderRadius="full"
+          boxSize="150px"
+          src="/images/tars.gif"
+          alt="tars"
+        />
+      </Center>
+      <Center>
+        <Text fontSize="3xl" fontWeight="extrabold">yoko</Text>
+      </Center>
+      <Center>
+        <Text fontSize="md" fontWeight="bold" color="gray.600">Backend Engineer</Text>
+      </Center>
+      <Center mt="7">
+        <Text fontSize="md" fontWeight="bold">My Skill 🔽</Text>
+      </Center>
+      <Center>
+        <Text fontSize="sm" color="gray.600" fontWeight="semibold">Ruby on Rails  |  React  |  TypeScript</Text>
+      </Center>
+    </Box>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,10 @@
-import { Box, Center, Image, Text } from "@chakra-ui/react"
+import NameCard from "../components/name_card"
 
 export default function Home() {
 
   return (
-    <Box
-      borderWidth="1px"
-      borderRadius="lg"
-      w="100%"
-      h="350px"
-      boxShadow="md"
-      p="6"
-      mt="8"
-      rounded="md"
-    >
-      <Center>
-        <Image
-          borderRadius="full"
-          boxSize="150px"
-          src="/images/tars.gif"
-          alt="Segun Adebayo"
-        />
-      </Center>
-      <Center>
-        <Text fontSize="3xl" fontWeight="extrabold">yoko</Text>
-      </Center>
-      <Center>
-        <Text fontSize="md" fontWeight="bold" color="gray.600">Backend Engineer</Text>
-      </Center>
-      <Center mt="6">
-        <Text fontSize="md" fontWeight="bold">My Skill 🔽</Text>
-      </Center>
-      <Center>
-        <Text fontSize="sm" color="gray.600" fontWeight="semibold">Ruby on Rails  |  React  |  TypeScript</Text>
-      </Center>
-    </Box>
+    <>
+      <NameCard />
+    </>
   )
 }
