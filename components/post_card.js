166c2ab64e4125fa0ce94261682cca5cf3a54e80
yokotadaiki <yd43star@gmail.com>
2021-10-29 19:28:36 +0900

    ダークモード修正

--- components/post_card.js
+++ components/post_card.js
@@ -1,10 +1,14 @@
-import { Box, Center, Image, Text, Flex, Spacer } from "@chakra-ui/react"
+import { Box, Center, Image, Text, Flex, Spacer, useColorMode } from "@chakra-ui/react"
 export default function PostCard(props) {
   const blog = props.blog
   const createdAt = new Date( blog.createdAt );
   const year = createdAt.getFullYear();
   const month = createdAt.getMonth() + 1;
   const day = createdAt.getDate();
+
+  const { colorMode } = useColorMode()
+  const text_color = colorMode === 'light' ? "gray.600" : "gray.300"
+  const bg_color   = colorMode === 'light' ? "gray.300" : "gray.600"
   return (
     <Flex>
       <Box
@@ -17,15 +21,15 @@ export default function PostCard(props) {
         mb="3"
         rounded="md"
       >
-        <Box bg="gray.300" w="100%" h="70px" p="3" />
+        <Box bg={bg_color} w="100%" h="70px" p="3" />
         <Box p="3">
           <Center>
-            <Text fontSize="3xl" fontWeight="bold">
+            <Text fontSize="3xl" fontWeight="bold" color={text_color}>
               {blog.title}
             </Text>
           </Center>
           <Box mt="7" textAlign="right">
-            <Text fontSize="xl" color="gray.600">
+            <Text fontSize="xl" color={text_color}>
               {year}/{month}/{day}
             </Text>
           </Box>
