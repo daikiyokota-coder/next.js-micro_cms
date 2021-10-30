c88deb26063f2b6769897d934dfd1ea05a43e230
yokotadaiki <yd43star@gmail.com>
2021-10-30 19:45:15 +0900

    3D環境構築

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,20 @@
-import { ChakraProvider, Container, useColorMode } from "@chakra-ui/react"
+import { ChakraProvider, Container, Box } from "@chakra-ui/react"
 import Header from "../components/header"
+import { Canvas } from 'react-three-fiber'
+import SampleBox from "../components/SampleBox"
+import Controls from "../utils/Controls"
 
 function MyApp({ Component, pageProps }) {
-  //const { colorMode, toggleColorMode } = useColorMode()
   return (
     <ChakraProvider>
       <Header />
+      <Box h="700px" w="500px">
+        <Canvas>
+          <SampleBox />
+          <Controls />
+          <gridHelper />
+        </Canvas>
+      </Box>
       <Container maxW="container.md">
         <Component {...pageProps} />
       </Container>
